ae0dce89db6a719e6a63583faeb9b31f56622314
Lucky Morifi <NOKOLUCKY19@gmail.com>
2025-11-06 15:51:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3640989321-ebeea",
+  "appId": "1:197695528055:web:1cd052b44fc42b34a8ba72",
+  "apiKey": "AIzaSyBjAtZWw4hP6rYMLOgSi74R0TcVgwk9duc",
+  "authDomain": "studio-3640989321-ebeea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "197695528055"
+};
